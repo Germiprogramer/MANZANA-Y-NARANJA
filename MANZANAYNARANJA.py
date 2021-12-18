8b86c0947a82b06e599b76af59bf2d3595d7ebf5
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-18 18:48:06 +0100

    introducir posicion

--- MANZANAYNARANJA.py
+++ MANZANAYNARANJA.py
@@ -1,1 +1,15 @@
+def manzanas_naranjas():
+    print("Elige el radio de la casa (dos números separados por un espacio, primero el menor valor)")
+    s,t = map(int, input().strip().split(' '))
+    print("Introduce la posición del manzano y del naranjo separadas por un espacio.")
+    a,b = map(int, input().strip().split(' '))
+    print("Introduce a la distancia a la que caen las manzanas del manzano y las naranjas del naranjo separadas por un espacio.")
+    m,n = map(int, input().strip().split(' '))
+    print("Introduce las distancias que hay entre cada par de manzanas separadas por un espacio.")
+    apple = list(map(int, input().strip().split(' ')))
+    print("Introduce las distancias que hay entre cada par de naranjas separadas por un espacio.")
+    orange = list(map(int, input().strip().split(' ')))
+
+manzanas_naranjas()
+
 
